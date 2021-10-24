cb9a8ae170877b159b5abd28ef172c5f76c19894
Kamil Kowalski <kamil@olicom.pl>
2021-10-24 18:47:04 +0200

    removing from list

--- app.js
+++ app.js
@@ -17,6 +17,9 @@ const app = Vue.createApp({
     addGoal() {
       this.goals.push(this.enteredGoalValue);
     },
+    removeGoal(index) {
+      this.goals.splice(index, 1);
+    },
   },
 });
 
